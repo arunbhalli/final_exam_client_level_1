a8ab55e68fe89f2a64ccc1f64ae36b26ed8d4793
arunbhalli <tomarbhalli@gmail.com>
2021-06-17 14:42:38 +0200

    add use effect and axios

--- src/components/ListOfMovies.jsx
+++ src/components/ListOfMovies.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 const ListOfMovies = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(async () => {
+    const result = await axios(
+      'https://content.viaplay.se/pc-se/serier/samtliga'
+    );
+    setMovies(result.data);
+  }, []);
+
   return (
     <div>
-      <h1>List of Movies</h1>
+      <h1 data-cy='list-of-movies'>List of Movies</h1>
+      <li>{movies}</li>
     </div>
-  )
-}
+  );
+};
 
-export default ListOfMovies
+export default ListOfMovies;
