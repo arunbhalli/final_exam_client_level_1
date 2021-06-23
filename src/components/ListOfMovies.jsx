b6332f8200ebec5f597aca3602bfd42668d2fb5c
arunbhalli <tomarbhalli@gmail.com>
2021-06-23 22:35:21 +0200

    adds  style to the fetch button

--- src/components/ListOfMovies.jsx
+++ src/components/ListOfMovies.jsx
@@ -1,14 +1,15 @@
-import React, {  useState } from 'react'
-import axios from "axios";
-import ReactDOM from "react-dom";
+import React, { useState } from 'react';
+import axios from 'axios';
+import ReactDOM from 'react-dom';
 // import { fetchSeries } from '../modules/viaplayService'
 const ListOfMovies = () => {
-  const [series, setSeries] = useState([])
-  const [error, setError] = useState(false)
-  const apiURL = 'https://content.viaplay.se/pc-se/serier/samtliga'
+  const [series, setSeries] = useState([]);
+  const apiURL = 'https://content.viaplay.se/pc-se/serier/samtliga';
   const fetchData = async () => {
     const response = await axios.get(apiURL);
-    setSeries(response.data._embedded['viaplay:blocks'][0]._embedded['viaplay:products']);
+    setSeries(
+      response.data._embedded['viaplay:blocks'][0]._embedded['viaplay:products']
+    );
   };
 
   // useEffect(() => {
@@ -20,14 +21,11 @@ const ListOfMovies = () => {
       <div data-cy='serie-card' className='display-show' key={index}>
         <img src={serie.content.images.landscape.url} alt={serie.publicPath} />
       </div>
-    )
-  })
+    );
+  });
 
   return (
     <div className='container'>
-    <button className="fetch-button" onClick={() => fetchData()}>
-    Fetch Data
-  </button>
       <div className='block'>
         <img
           data-cy='logo'
@@ -36,11 +34,11 @@ const ListOfMovies = () => {
           src='https://kundservice.viaplay.se/wp-content/themes/viaplaycs/assets/dist/images/viaplay_white.svg'
         />
       </div>
-      <div className='series-container'>
-          {listOfSeries}
-      </div>
-
+      <button className='fetch-button' onClick={() => fetchData()}>
+        Fetch Data
+      </button>
+      <div className='series-container'>{listOfSeries}</div>
     </div>
-  )
-}
+  );
+};
 export default ListOfMovies;
