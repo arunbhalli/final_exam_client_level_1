7374bd275570cdeefd8e0df71ecd673bddc0fb94
arunbhalli <tomarbhalli@gmail.com>
2021-06-27 19:04:24 +0200

    adds no-of-series and name-of-series modified test

--- src/components/ListOfMovies.jsx
+++ src/components/ListOfMovies.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import ReactDOM from 'react-dom';
 // import { fetchSeries } from '../modules/viaplayService'
 const ListOfMovies = () => {
   const [series, setSeries] = useState([]);
   const apiURL = 'https://content.viaplay.se/pc-se/serier/samtliga';
+
   const fetchData = async () => {
     const response = await axios.get(apiURL);
     setSeries(
@@ -18,9 +18,19 @@ const ListOfMovies = () => {
 
   const listOfSeries = series.map((serie, index) => {
     return (
-      <div data-cy='serie-card' className='display-show' key={index}>
-        <img src={serie.content.images.landscape.url} alt={serie.publicPath} />
-      </div>
+        <div data-cy='series-length' className='display-show' key={index}>
+        <div data-cy='img-of-series'>
+           <img
+            src={serie.content.images.landscape.url}
+            alt={serie.publicPath}
+          />
+        </div>
+         
+          <div>
+            <p data-cy='title'>Name: {serie.content.series.title}</p>
+            <p data-cy='no-of-series'>Total rleased series:{serie.content.series.seasons}</p>
+          </div>
+        </div>
     );
   });
 
@@ -34,10 +44,15 @@ const ListOfMovies = () => {
           src='https://kundservice.viaplay.se/wp-content/themes/viaplaycs/assets/dist/images/viaplay_white.svg'
         />
       </div>
-      <button className='fetch-button' onClick={() => fetchData()}>
+      <button
+        className='fetch-button'
+        data-cy='fetch-data'
+        onClick={() => fetchData()}>
         Fetch Data
       </button>
-      <div className='series-container'>{listOfSeries}</div>
+      <div className='series-container' data-cy='list-of-series'>
+        {listOfSeries}
+      </div>
     </div>
   );
 };
